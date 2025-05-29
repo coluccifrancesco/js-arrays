d91090e4987f35658de089e6befa1b449f3b802c
coluccifrancesco <coluccif2002@gmail.com>
2025-05-29 19:12:27 +0200

    Refactor array manipulation logic and improve console output for teachers

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,3 +1,5 @@
+// http://127.0.0.1:3000/esercizi/js-arrays/02-arrays-bonus/index.html
+
 const teachers = [
   'Nathan',
   'Ed',
@@ -11,11 +13,7 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.reverse();
-
-for (let i = 0; i < teachers.length; i++){
-  const teachersElements = teachers[i];
-  console.log(teachersElements);
-}
+console.log(reversedTeachers)
 
 console.log('')
 console.log('')
@@ -28,18 +26,37 @@ console.log('')
 console.log('')
 
 // 3. Rimuovi 'Ed' dall'array teachers
-const noEd = teachers.splice('Ed', 1);
-console.log(noEd)
+const edIndex = teachers.indexOf("Ed");
+
+if (edIndex !== -1){
+  teachers.splice(edIndex, 1)
+  
+  for(let i = 0; i < teachers.length; i++){
+    teachersCounter = teachers[i];
+    console.log(teachersCounter)
+  }
+}
 
 console.log('')
 console.log('')
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio');
+
+if(isFabioPresent){
+  for(let i = 0; i < teachers.length; i++){
+    teachersCounter = teachers[i];
+    console.log(teachersCounter)
+  }
+}
 
 console.log('')
 console.log('')
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
+
+// Per ora ho fatto questi, ed oltretutto devo essere sincer0, alcuni dubbi di logica me li sono tolti con copilot
+// non mi ha fatto linee di codice intere, piuttosto ha risolto problemi di sintassi.
+// So che Artur e Fabio sono molto attenti all'uso di questo strumento quindi preferisco essere sincero
